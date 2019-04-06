d19f5e9b518227daf5e939660144d9950418e18f
Lenninlasd <lenninlasd@gmail.com>
2019-04-06 16:37:04 -0500

    Add players component styles

--- src/Players/index.js
+++ src/Players/index.js
@@ -5,6 +5,8 @@ import PropTypes from 'prop-types';
 import { addPlayers } from '../Actions/players.js';
 import { setLevel } from '../Actions/config.js';
 
+import styles from '../Styles/players.css';
+
 class Players extends Component {
   constructor(props) {
     super(props);
@@ -34,19 +36,43 @@ class Players extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Enter Player&apos;s Names</h1>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <label htmlFor="player1">Player 1</label>
-            <input required id="player1" onChange={this.handleChange} value={this.state.player1} />
-          </div>
-          <div>
-            <label htmlFor="player2">Player 2</label>
-            <input required id="player2" onChange={this.handleChange} value={this.state.player2} />
-          </div>
-          <button type="submit">Start</button>
-        </form>
+      <div className={styles.playersContainer}>
+        <div className={styles.playersCard}>
+          <h1>Enter Player&apos;s Names</h1>
+          <form onSubmit={this.handleSubmit}>
+            <div className={styles.row}>
+              <label className={styles.label2} htmlFor="player1">
+                Player 1
+              </label>
+              <div className={styles.layout8}>
+                <input
+                  className={styles.inputPlayer}
+                  required
+                  id="player1"
+                  onChange={this.handleChange}
+                  value={this.state.player1}
+                />
+              </div>
+            </div>
+            <div className={styles.row}>
+              <label className={styles.label2} htmlFor="player2">
+                Player 2
+              </label>
+              <div className={styles.layout8}>
+                <input
+                  className={styles.inputPlayer}
+                  required
+                  id="player2"
+                  onChange={this.handleChange}
+                  value={this.state.player2}
+                />
+              </div>
+            </div>
+            <button className={`${styles.btn} ${styles.btnBlock}`} type="submit">
+              Start
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
